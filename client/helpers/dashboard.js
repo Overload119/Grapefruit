8d4c41f52e860a5d2394a2dc4795553d4af4a5b2
Overload119 <budgeneration@gmail.com>
2014-11-22 12:57:32 -0500

    Add async search functionality using GeoNear

--- client/helpers/dashboard.js
+++ client/helpers/dashboard.js
@@ -1,10 +1,44 @@
 Template.dashboard.helpers({
   isTutorialComplete: function() {
-    return Meteor.user().isTutorialComplete;
+    // This is a bit slow, so we check the localStorage as well.
+    // This is faster than Meteor.user() which would show a flash on first load.
+    var slowCheck = Meteor.user().isTutorialComplete;
+    if (localStorage.getItem('isTutorialComplete')) {
+      return true;
+    }
+    if (slowCheck) {
+      localStorage.setItem('isTutorialComplete', true);
+    }
+    return slowCheck;
   },
   isLoginConfigured: function() {
     return Accounts.loginServicesConfigured();
+  },
+  isSearching: function() {
+    return Session.get('searchQuery').trim() !== '';
+  },
+});
+
+Template.dashboard.events({
+  'keyup #search-bar': function(evt, template) {
+    // If they hit Enter, start a search.
+    if (evt.keyCode === 13) {
+      var searchQueryParams = {
+        term: $(evt.currentTarget).val(),
+        lat: Meteor.user().location[0],
+        lng: Meteor.user().location[1]
+      };
+
+      Meteor.call('searchQuery', searchQueryParams, function(err, result) {
+        console.log(err);
+        console.log(result);
+      });
+    }
   }
 });
+
+Template.dashboard.created = function() {
+  Session.setDefault('searchQuery', '');
+}
 
 Meteor.subscribe('currentUserProfile');
---
+++ server/search.js
@@ -0,0 +1,62 @@
+var _db, _users, MongoClient;
+
+MongoClient = Meteor.npmRequire('mongodb').MongoClient;
+MongoClient.connect('mongodb://127.0.0.1:3001/meteor', function(err, db) {
+  if (err) {
+    throw err;
+  }
+  _db = db; // make database handle available to Meteor.methods
+})
+
+Meteor.methods({
+  searchQuery: function(searchParams, callback) {
+    // Look through skills and work experience.
+    // Sort by distance (using $near forces this).
+    // Limit of 100 is also enforced by $near.
+    // TODO Incorporate recently active
+    check(searchParams, Object);
+
+
+
+    if (!this.userId) {
+      throw new Meteor.Error('User is not logged in');
+    }
+
+    if (!searchParams.term) {
+      throw new Meteor.Error('Search parameters did not contain `term`.');
+    }
+
+    var asyncDbLookup = function(callback) {
+      _db.command({
+        geoNear: 'users',
+        near: [searchParams.lat, searchParams.lng],
+        limit: 30,
+        query: {
+          $or: [
+            { interests: searchParams.term },
+            { skills: searchParams.term },
+            { jobExperience: searchParams.term }
+          ]
+        }
+      }, function(err, res) {
+        var results = [];
+
+        // Pluck only the fields we want.
+        if (res.results && res.results.length > 0) {
+          results = _.map(res.results, function(entry) {
+            var mappedEntry = _.pick(entry.obj, '_id', 'pictureUrl', 'headline', 'firstName');
+            mappedEntry.distance = entry.dis;
+
+            return mappedEntry
+          });
+        }
+
+        console.log(results);
+        callback(null, results);
+      });
+    };
+
+    var syncDbLookup = Meteor.wrapAsync(asyncDbLookup);
+    return syncDbLookup();
+  }
+});
