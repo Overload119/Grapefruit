c0e290a7bf2462e769ddb1b582dfc6bc61b76df6
Overload119 <budgeneration@gmail.com>
2014-12-04 16:06:53 -0500

    Add ability to add/remove skills and interests

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,9 +1,24 @@
 $.fn.enable = function() {
   this.removeClass('disabled').removeProp('disabled');
   return this;
-}
+};
 
 $.fn.disable = function() {
   this.addClass('disabled').prop('disabled', 'disabled');
   return this;
-}
+};
+
+$.fn.scrollToBottom = function(duration) {
+  if (!this[0]) {
+    throw new Error('scrollToBottom called with no element.');
+  }
+
+  if (duration) {
+    this.animate({
+      scrollTop: this[0].scrollHeight
+    }, duration);
+  } else {
+    this.scrollTop( this[0].scrollHeight );
+  }
+  return this;
+};
--- client/views/users/settings.js
+++ client/views/users/settings.js
@@ -41,6 +41,56 @@ Template.usersSettings.events({
       el.find('.text').html('Save');
     }
   },
+  'keyup .tag-textbox': function(evt, template) {
+    // Enter is a shortcut to submit.
+    if (evt.keyCode === 13) {
+      el = $(evt.currentTarget);
+      el.siblings('.tag-submit').click();
+    }
+  },
+  'click .tag-submit': function(evt, template) {
+    var el = $(evt.currentTarget);
+    var txtbox = el.siblings('.tag-textbox');
+    var userKey = el.closest('.list-card').data('user-key');
+    var payload = {}
+    payload[ userKey ] = txtbox.val();
+
+    if (txtbox.val() === '') {
+      return;
+    }
+
+    if (_.contains(template.data.interests, txtbox.val().toLowerCase())) {
+      txtbox.notify(txtbox.val() + ' has already been added.', { position: 'top left' });
+      txtbox.val('');
+      return;
+    }
+
+    txtbox.val('');
+
+    Meteor.users.update({ _id: this._id }, {
+      $addToSet: payload
+    });
+
+    // Scroll to bottom if possible.
+    var newEl = el.closest('.list-card').find('.list li:last').addClass('appear');
+    el.closest('.list-card').find('.list').scrollToBottom(500);
+    Meteor.defer(function() {
+      newEl.removeClass('appear');
+    });
+  },
+  'click .remove-tag-btn': function(evt, template) {
+    el = $(evt.currentTarget);
+    var tag = el.data('tag');
+    var userKey = el.closest('.list-card').data('user-key');
+    var payload = {};
+    payload[ userKey ] = tag;
+
+    // Must use template.data here because `this` changed scope since this element is in a
+    // #each loop.
+    Meteor.users.update({ _id: template.data._id }, {
+      $pull: payload
+    });
+  },
   'click .role': function(evt, template) {
     var el        = $(evt.currentTarget);
     var dataRole  = el.data('role');
