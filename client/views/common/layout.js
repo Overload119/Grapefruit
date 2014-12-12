ad3247b6fe6ef53b03b44c64c3d1cd1019c708cc
Overload119 <budgeneration@gmail.com>
2014-12-12 11:13:16 -0500

    Refactor by adding a events/projects as their own section and a staticLayout template

--- client/views/common/layout.js
+++ client/views/common/layout.js
@@ -1,8 +1,5 @@
 Template.layout.helpers({
   pageName: getCurrentTemplate,
-  shouldShowNav: function() {
-    return Session.get('shouldShowNav');
-  },
   toggleActiveRouteNav: function(routeToCheck) {
     var currentRouteName = Router.current().lookupTemplate().toLowerCase();
     routeToCheck = routeToCheck.toLowerCase();
@@ -10,3 +7,13 @@ Template.layout.helpers({
     return currentRouteName === routeToCheck ? 'active' : '';
   }
 });
+
+Template.layout.rendered = function() {
+  $('.nav-left a').tooltipster({
+    theme: 'tooltipster-light',
+    contentAsHTML: true,
+    animation: 'grow',
+    speed: 250,
+    position: 'left'
+  });
+}
